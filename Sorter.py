4b43200895ed1b667dc095944abef3412d1485e0
Chinmay Bhide <Chinmay.Bhide154@gmail.com>
2022-07-13 22:34:15 -0700

    Adding insertion sort

--- Sorter.py
+++ Sorter.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 
+
 class Sorter:
     def __init__(self, list_values, arranged_values, num_values, max_value, min_value, pause_time):
         self.list_values = list_values
@@ -20,3 +21,20 @@ class Sorter:
                     self.list_values[j], self.list_values[j + 1] = self.list_values[j + 1], self.list_values[j]
 
         plt.show()
+
+    def insertion_sort(self):
+        for i in range(1, self.num_values):
+            key_item = self.list_values[i]
+            j = i - 1
+            while j >= 0 and self.list_values[j] > key_item:
+                self.list_values[j + 1] = self.list_values[j]
+                j -= 1
+
+                plt.bar(self.arranged_values, self.list_values)
+                plt.pause(self.pause_time)
+                plt.clf()
+
+            self.list_values[j + 1] = key_item
+
+
+
--- main.py
+++ main.py
@@ -2,9 +2,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 from Sorter import Sorter
 
+
 num_values = 15
-lst = np.random.randint(0, 100, num_values)
+values = np.random.randint(0, 100, num_values)
 x = np.arange(0, num_values, 1)
 
-sorter = Sorter(lst, x, num_values, 100, 0, 0.01)
+sorter = Sorter(values, x, num_values, 100, 0, 0.01)
+sorter.insertion_sort()
 sorter.bubble_sort()
